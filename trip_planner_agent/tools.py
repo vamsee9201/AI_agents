4077996252482a5dcf2c253524405ec5aac5dfff
vamsee9201 <vamseekrishna9201@gmail.com>
2025-10-07 21:31:01 -0500

    added directions tool for the agent

--- trip_planner_agent/tools.py
+++ trip_planner_agent/tools.py
@@ -2,8 +2,14 @@
 import requests
 import json
 
-def get_route_info():
-    # Load API key from JSON file
+def get_directions(origin:str, destination:str):
+    """
+    fetches the directions between two locations.
+
+    Args:
+        origin (str): The origin location.
+        destination (str): The destination location.
+    """
     with open("google_maps_api.json", "r") as f:
         key_data = json.load(f)
         api_key = key_data["api_key"]
@@ -11,8 +17,8 @@ def get_route_info():
     # API endpoint and parameters
     url = "https://maps.googleapis.com/maps/api/directions/json"
     params = {
-        "origin": "2726 phillips dr dallas",
-        "destination": "dfw airport",
+        "origin": origin,
+        "destination": destination,
         "key": api_key
     }
 
@@ -33,7 +39,8 @@ def get_route_info():
     print(f"Distance: {distance_text}")
     print(f"Duration: {duration_text}")
 
-if __name__ == "__main__":
-    get_route_info()
+
 
 # %%
+# if __name__ == "__main__":
+#     get_directions("2726 phillips dr dallas", "dfw airport")
